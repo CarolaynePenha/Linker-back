7925340dd75fbc563a49ce6a8fb6d4d6cd870cd1
Carolayne <carolaynepenha@gmail.com>
2023-12-14 12:19:35 -0300

    fix: error message

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -7,6 +7,11 @@ export async function signUp(req, res) {
 
   const passwordHash = bcrypt.hashSync(password, 10);
   try {
+    const UserExist = await userRepositories.getUserByEmail(email);
+    console.log("UserExist.rowCount: ", UserExist.rowCount);
+    if (UserExist.rowCount !== 0) {
+      return res.status(409).send("e-mail already exists");
+    }
     const querySignUp = await userRepositories.saveUserInfos(
       name,
       email,
@@ -24,15 +29,15 @@ export async function signIn(req, res) {
   const { password, email } = req.body;
 
   try {
-    const queryInfoUser = await userRepositories.getUserByEmail(email);
+    const infoUser = await userRepositories.getUserByEmail(email);
     if (
-      queryInfoUser.rowCount === 1 &&
-      bcrypt.compareSync(password, queryInfoUser.rows[0].password)
+      infoUser.rowCount === 1 &&
+      bcrypt.compareSync(password, infoUser.rows[0].password)
     ) {
       const token = uuid();
-      const { id } = queryInfoUser.rows[0];
+      const { id, name, image } = infoUser.rows[0];
       const queryToken = await userRepositories.createSession(token, id);
-      res.status(200).send(token);
+      res.status(200).send({ token: token, name: name, image: image });
     } else {
       res.status(404).send("user not found");
     }
