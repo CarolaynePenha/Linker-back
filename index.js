286eac858729f6f96905168526dbd407969e04c4
Carolayne <carolaynepenha@gmail.com>
2023-12-14 10:08:33 -0300

    fix: cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import router from "./routers/index.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
 
 app.use(json());
+app.use(cors());
 app.use(router);
 
 const port = process.env.PORT || 5500;
